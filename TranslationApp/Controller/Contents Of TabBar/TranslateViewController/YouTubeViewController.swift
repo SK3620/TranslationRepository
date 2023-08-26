e59ed000ca5c63ee0cdae916e05edd7e3a9b72fd
SK3620 <jiantail519@gmail.com>
2023-08-26 19:38:15 +0900

    YT動画JSONデータに字幕情報はなし

--- TranslationApp/Controller/Contents Of TabBar/TranslateViewController/YouTubeViewController.swift
+++ TranslationApp/Controller/Contents Of TabBar/TranslateViewController/YouTubeViewController.swift
@@ -5,25 +5,15 @@
 //  Created by 鈴木健太 on 2023/08/25.
 //
 
+import Alamofire
+import Foundation
 import UIKit
 
 class YouTubeViewController: UIViewController {
+    @IBOutlet var youTubeLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
