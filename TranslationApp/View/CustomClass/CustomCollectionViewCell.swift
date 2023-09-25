4eb7a4a63cfb3e59f13fca901dbf6c521a00fc72
SK3620 <jiantail519@gmail.com>
2023-09-25 22:44:42 +0900

    CustomCollectionViewCellクラスを作成

---
+++ TranslationApp/View/CustomClass/CustomCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CustomCollectionViewCell.swift
+//  TranslationApp
+//
+//  Created by 鈴木健太 on 2023/09/25.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+}
